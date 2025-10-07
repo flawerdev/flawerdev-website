18491ae1ab6c6951aa19d4d5b165c9035b0ebccc
flawerdev <145572948+flawerdev@users.noreply.github.com>
2025-10-07 12:48:11 -0300

    Prep for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://token-template.deno.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+
   markdown: {
     shikiConfig: {
       theme: "github-dark-high-contrast",
     },
   },
+
+  adapter: vercel(),
 });
