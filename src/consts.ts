840cafc2281694e3bc40c08e56d3e26ee7743f74
flawerdev <vfloresseguel@gmail.com>
2025-10-09 13:13:38 -0300

    update idioma, agregar contact card

--- src/consts.ts
+++ src/consts.ts
@@ -10,6 +10,7 @@ export const ABOUT_ME =
   `Hola! soy flawer, analista de sistemas y desarrollo de software,  I’m a software developer based in Chile, con formación previa en enseñanza de inglés. Descubrí la programación durante la pandemia a través de comunidades de coworking en Twitch, lo que inspiró un cambio de rumbo profesional. Disfruto de desarrollar proyectos prácticos y bien pensados, explorando ideas a mi propio ritmo y a mi manera. Me apasiona aprender nuevas cosas y el crecimiento continuo. Más allá de la programación, disfruto de la música, crear playlists y compartir covers de guitarra. Mi objetivo es crear un espacio acogedor donde la tecnología, la creatividad y la curiosidad se encuentren y se potencien mutuamente.`.trim();
 export const GITHUB_USERNAME = "flawerdev";
 export const QUOTE = "Programmer, Gamer, Streamer";
+export const CONTACT_EMAIL = "flawerdev@gmail.com"
 export const NAV_LINKS: Array<{ title: string; href?: string }> = [
   {
     title: "Blog",
